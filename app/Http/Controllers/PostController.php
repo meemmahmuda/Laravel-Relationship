9c95b14bb89253a0ad3d9ef67f45aec3a6943970
meemmahmuda <meemmahmuda85@gmail.com>
2024-08-29 10:58:15 +0600

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Like;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index (){
+    $post = Post::with('like')->get();
+    return view('post', compact('post'));
+}
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Role;
+
+class UserController extends Controller
+{
+    public function index () {
+    $role =Role::with('user')->get();
+    // dd($users);
+    // return $users;
+    return view('user', compact('role'));
+    }
+}
